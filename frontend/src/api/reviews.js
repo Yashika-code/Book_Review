933130f6ee322297143d208d79847a72c513e159
Yashika soni <soniyashika164@gmail.com>
2025-10-05 10:10:04 +0530

    Update reviews.js

--- frontend/src/api/reviews.js
+++ frontend/src/api/reviews.js
@@ -1,17 +1,17 @@
 import axiosInstance from './axios';
 
 export const addReview = async (bookId, reviewData) => {
-  const response = await axiosInstance.post(`/reviews/${bookId}`, reviewData);
+  const response = await axiosInstance.post(`/api/reviews/${bookId}`, reviewData);
   return response.data;
 };
 
 export const updateReview = async (reviewId, reviewData) => {
-  const response = await axiosInstance.put(`/reviews/${reviewId}`, reviewData);
+  const response = await axiosInstance.put(`/api/reviews/${reviewId}`, reviewData);
   return response.data;
 };
 
 export const deleteReview = async (reviewId) => {
-  const response = await axiosInstance.delete(`/reviews/${reviewId}`);
+  const response = await axiosInstance.delete(`/api/reviews/${reviewId}`);
   return response.data;
 };
 
